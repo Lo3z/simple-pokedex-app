ff0c403647574c82581dc96432261784e4d207af
Lo3z <carlosheins1@gmail.com>
2025-06-04 12:51:14 -0400

    replaced for look with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 2.04, types: ['Grass', 'Poison']},
-    {name: 'Ivysaur', height: 3.03, types: ['Grass', 'Poison']},
-    {name: 'Venusaur', height: 6.07, types: ['Grass', 'Poison']},
+    {name: 'Bulbasaur', height: 2.04, types: ['Grass', ' Poison']},
+    {name: 'Ivysaur', height: 3.03, types: ['Grass', ' Poison']},
+    {name: 'Venusaur', height: 6.07, types: ['Grass', ' Poison']},
     {name: 'Charmander', height: 2, types: ['Fire']},
     {name: 'Charmeleon', height: 3.07, types: ['Fire']},
-    {name: 'Charizard', height: 5.07, types: ['Fire', 'Flying']},
+    {name: 'Charizard', height: 5.07, types: ['Fire', ' Flying']},
     {name: 'Squirtle', height: 1.08, types: ['Water']},
     {name: 'Wartortle', height: 3.03, types: ['Water']},
     {name: 'Blastoise', height: 5.03, types: ['Water']},
 ];
 
-for (let i = 0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')');
-    if (pokemonList[i].height >= 5){
-        document.write(" - Wow, that's big!")
+pokemonList.forEach(function(pokemon){
+    document.write(pokemon.name + ' ' + pokemon.types + ' ' + pokemon.height);
+    if(pokemon.height >= 5) {
+        document.write(" - Wow, that's big!");
     }
     document.write("<br>")
-}
+});
