4c1e0b5e22ca8f79f19b6a949e6a2b7ee7f5ca98
Lo3z <carlosheins1@gmail.com>
2025-06-05 12:17:52 -0400

    added a button list and a header to the pokedex
    Used DOM query selector to add a list of pokemon buttons to the index, attempting to have buttons return pokemon names to the console.

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,21 @@ let pokemonRepository = (function(){
     {name: 'Blastoise', height: 5.03, types: ['Water']},
     ];
 
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemonButton');
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);
+        button.addEventListener('click', showDetails(pokemon));
+    }
+
+    function showDetails(pokemon){
+        console.log(pokemon.name);
+    }
+
     return {
         add: function(pokemon) {
             pokemonList.push(pokemon);
@@ -18,15 +33,12 @@ let pokemonRepository = (function(){
         getAll: function() {
         return pokemonList;
         },
+        addListItem: addListItem
     }
 })();
 
 pokemonRepository.add({name: 'Pikachu', height: '1.04', types: ['Electric']});
 
 pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name + ' ' + pokemon.types + ' ' + pokemon.height);
-    if(pokemon.height >= 5) {
-        document.write(" - Wow, that's big!");
-    }
-    document.write("<br>")
+    pokemonRepository.addListItem(pokemon);
 });
